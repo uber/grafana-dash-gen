d8705820436211e22badd324dc002e926afed57a
Evan Culver <eculver@uber.com>
2015-06-30 20:00:52 -0700

    Coverage for row module

--- grafana/row.js
+++ grafana/row.js
@@ -1,31 +1,41 @@
 'use strict';
 
-function Row(opts) {
-	opts = opts || {};
-	
-	this.state = {
-      'title': 'New row',
-      'height': '250px',
-      'editable': true,
-      'collapse': false,
-      'panels': []
-	};
+var xtend = require('xtend');
 
-	this.panels = [];
+function Row(opts) {
+    opts = opts || {};
+    var self = this;
+
+    var state = {
+      title: 'New row',
+      height: '250px',
+      editable: true,
+      collapse: false,
+      panels: []
+    };
+
+    this.state = xtend(state, opts);
+    this.panels = [];
+
+    if (opts.panels) {
+        opts.panels.forEach(function addP(panel) {
+            self.addPanel(panel);
+        })
+    }
 }
 
 Row.prototype.generate = function generate() {
-	var generatedJson = [];
-	this.panels.forEach(function generatePannelJson(panel) {
-		generatedJson.push(panel.generate());
-	});
+    var generatedJson = [];
+    this.panels.forEach(function generatePannelJson(panel) {
+        generatedJson.push(panel.generate());
+    });
 
-	this.state.panels = generatedJson;
-	return this.state;
+    this.state.panels = generatedJson;
+    return this.state;
 };
 
 Row.prototype.addPanel = function addPanel(panel) {
-	this.panels.push(panel);
+    this.panels.push(panel);
 };
 
 module.exports = Row;
