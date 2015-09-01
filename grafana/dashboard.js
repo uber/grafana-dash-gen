ee39c969c2ebb77c7df3700917b147db4386bf73
Evan Culver <eculver@uber.com>
2015-09-01 12:38:30 -0700

    Initial support for Grafana 2, still some chunks that need addressing, especially in the case for updating dashboards

--- grafana/dashboard.js
+++ grafana/dashboard.js
@@ -24,11 +24,34 @@ var Templates = require('./templates');
 var Annotations = require('./annotations');
 
 function Dashboard(opts) {
-    opts = opts || {};
     var self = this;
+    opts = opts || {};
 
+    // initialize internal state
     this.state = {};
+    this._init(opts);
+    this._initMeta(opts);
+    this._initRows(opts);
+    this._initAnnotations(opts);
+    this._initTemplating(opts);
 
+    if (opts.templating) {
+        opts.templating.forEach(function temp(template) {
+            template = new Templates.Custom(template);
+            self.addTemplate(template);
+        });
+    }
+
+    if (opts.annotations) {
+        opts.annotations.forEach(function temp(annotation) {
+            annotation = new Annotations.Graphite(annotation);
+            self.addAnnotation(annotation);
+        });
+    }
+}
+
+Dashboard.prototype._init = function _init(opts) {
+    this.state = this.state || {};
     this.state.id = opts.id || null;
     this.state.title = opts.title || 'Generated Grafana Dashboard';
     this.state.originalTitle = opts.originalTitle || 'Generated Dashboard';
@@ -39,45 +62,33 @@ function Dashboard(opts) {
     this.state.hideControls = !!opts.hideControls;
     this.state.sharedCrosshair = !!opts.sharedCrosshair;
     this.state.refresh = opts.refresh || false;
-    this.state.version = opts.version || 6;
+    this.state.schemaVersion = opts.schemaVersion || 6;
     this.state.hideAllLegends = !!opts.hideAllLegends;
+};
 
+Dashboard.prototype._initMeta = function _initMeta(opts) {
     this.state.meta = {};
-    this.state.meta.slug = opts.slug || this.state.title.split(' ').join('-');
+    this.state.slug = opts.slug || this.state.title.split(' ').join('-');
+};
 
+Dashboard.prototype._initRows = function _initMeta(opts) {
+    this.rows = [];
     this.state.rows = [];
+};
 
+Dashboard.prototype._initAnnotations = function _initAnnotations(opts) {
     this.state.annotations = {
         list: [],
         enable: true
     };
+};
 
+Dashboard.prototype._initTemplating = function _initTemplating(opts) {
     this.state.templating = {
         list: [],
         enable: true
     };
-
-    this.state.annotations = {
-        list: [],
-        enable: true
-    };
-
-    if (opts.templating) {
-        opts.templating.forEach(function temp(template) {
-            template = new Templates.Custom(template);
-            self.addTemplate(template);
-        });
-    }
-
-    if (opts.annotations) {
-        opts.annotations.forEach(function temp(annotation){
-            annotation = new Annotations.Graphite(annotation);
-            self.addAnnotation(annotation);
-        });
-    }
-
-    this.rows = [];
-}
+};
 
 Dashboard.prototype.addTemplate = function addTemplate(template) {
     this.state.templating.list.push(template.generate());
@@ -93,13 +104,13 @@ Dashboard.prototype.addRow = function addRow(row) {
 
 Dashboard.prototype.generate = function generate() {
     // Generate json for the rows
-    var generatedJson = [];
+    var generatedRows = [];
     this.rows.forEach(function generateRowJson(row) {
-        generatedJson.push(row.generate());
+        generatedRows.push(row.generate());
     });
 
-    this.state.rows = generatedJson;
-    return JSON.stringify(this.state, null, '\t');
+    this.state.rows = generatedRows;
+    return this.state;
 };
 
 module.exports = Dashboard;
--- grafana/publish.js
+++ grafana/publish.js
@@ -23,9 +23,10 @@ var request = require('request');
 var config = require('./config');
 var errors = require('./errors');
 
-// Utility method for determining whether a string ends with a specific character
+// Utility method for determining whether a string ends with a
+// specific character
 function endsWith(str, char) {
-    return str.lastIndexOf(char) === str.length-1;
+    return str.lastIndexOf(char) === str.length - 1;
 }
 
 function publish(dashboard) {
@@ -47,7 +48,7 @@ function publish(dashboard) {
         });
     }
 
-    if (!state.meta.slug) {
+    if (!state.slug && !state.meta.slug) {
         throw errors.InvalidState({
             component: 'grafana.Dashboard',
             invalidArg: 'meta.slug',
@@ -84,13 +85,9 @@ function publish(dashboard) {
         });
     }
 
-    var separator = endsWith(cfg.url, '/') ? '' : '/';
-    var putUrl = [cfg.url, state.meta.slug].join(separator);
-    var putData = {
-        user: cfg.user,
-        group: cfg.group,
-        title: state.title,
-        tags: state.tags,
+    // var separator = endsWith(cfg.url, '/') ? '' : '/';
+    // var deleteUrl = [cfg.url, state.slug].join(separator);
+    var createData = {
         dashboard: dashboard.generate()
     };
 
@@ -99,18 +96,35 @@ function publish(dashboard) {
     j.setCookie(cookie, cfg.url);
 
     request({
-        url: putUrl,
-        method: 'PUT',
-        json: putData,
+        url: cfg.url,
+        method: 'POST',
+        json: createData,
         jar: j
-    }, function responseHandler(err, response) {
-        if (err) {
+    }, function createResponseHandler(createErr, rawCreateResp) {
+        var createResp = rawCreateResp.toJSON();
+        if (createErr) {
             console.log('Unable to publish dashboard ' + state.title);
+        } else if (createResp.statusCode === 412) {
+            console.log('Unable to publish dashboard: ' + state.title + ' already exists');
+            /* TODO: should delete here?
+            request({
+              url: deleteUrl,
+              method: 'DELETE',
+              jar: j
+            }, function deleteResponseHandler(deleteErr, rawDeleteResp) {
+                if (deleteErr) {
+                    console.log('Unable to publish dashboard ' + state.title);
+                }
+                var deleteResp = rawDeleteResp.toJSON();
+                console.log(deleteResp.statusCode);
+                // if success, re-publish
+            });
+            */
         } else {
-            if ([200, 201].indexOf(response.statusCode) === -1) {
+            if ([200, 201].indexOf(createResp.statusCode) === -1) {
                 console.log('Unable to publish dashboard ' + state.title);
-                console.log(response.body);
-                console.log('Got statusCode' + response.statusCode);
+                console.log(createResp.body);
+                console.log('Got statusCode' + createResp.statusCode);
                 console.log('An invalid auth token results in a 302 error!');
             } else {
                 console.log('Published the dashboard ' + state.title);
