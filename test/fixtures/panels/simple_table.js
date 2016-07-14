8be6a74c90c083347b01fe23ce3a39c56735eab2
Nitin Shyamkumar <nitins@uber.com>
2016-07-14 13:14:53 -0700

    adds support for tables with test coverage

--- test/fixtures/panels/simple_table.js
+++ test/fixtures/panels/simple_table.js
@@ -18,45 +18,46 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-'use strict';
-
-var generateGraphId = require('../id');
-
-function Text(opts) {
-    opts = opts || {};
-    var self = this;
-
-    this.state = {
-        title: '',
-        error: false,
-        span: 12,
-        editable: true,
-        type: 'text',
-        mode: 'markdown',
-        content: '',
-        style: {},
-        links: []
-    };
-
-    this.state.title = opts.title || this.state.title;
-    this.state.id = opts.id || generateGraphId();
-    this.state.span = opts.span || 12;
-    this.state.content = opts.content || this.state.content;
-	this.state.mode = opts.mode || this.state.mode;
-
-    // finally add to row/dashboard if given
-    if (opts.row && opts.dashboard) {
-        opts.row.addPanel(this);
-        opts.dashboard.addRow(opts.row);
-    }
-}
-
-Text.prototype.generate = function generate() {
-    return this.state;
+module.exports = {
+    title: "Panel Title",
+    error: false,
+    span: 12,
+    editable: true,
+    type: "table",
+    isNew: true,
+    id: 1,
+    styles: 
+    [{
+        type: "date",
+        pattern: "Time",
+        dateFormat: "YYYY-MM-DD HH:mm:ss"
+    },{
+        unit: "short",
+        type: "number",
+        decimals: 0,
+        colors: [
+            "rgba(245, 54, 54, 0.9)",
+            "rgba(237, 129, 40, 0.89)",
+            "rgba(50, 172, 45, 0.97)"
+        ],
+        colorMode: null,
+        pattern: "/.*/",
+        thresholds: []
+    }],
+    targets: [],
+    transform: "timeseries_aggregations",
+    pageSize: null,
+    showHeader: true,
+    columns: 
+    [{
+        text: "Avg",
+        value: "avg"
+    }],
+    scroll: true,
+    fontSize: "100%",
+    sort: {
+        col: 0,
+        desc: true
+    },
+    links: []
 };
-
-Text.prototype.setTitle = function setTitle(title) {
-    this.state.title = title;
-};
-
-module.exports = Text;
