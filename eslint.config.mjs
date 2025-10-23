c0f4e4c3401ba971d63fe6549824352582c0b13e
Bogdan Savluk <savluk.bogdan@gmail.com>
2025-10-23 22:28:11 +0200

    prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,22 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  tseslint.configs.recommended,
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        languageOptions: { globals: globals.node },
+    },
+    tseslint.configs.recommended,
+    eslintPluginPrettierRecommended,
     {
         rules: {
             '@typescript-eslint/no-require-imports': 'off',
-            '@typescript-eslint/no-this-alias': 'off'
-        }
-    }
+            '@typescript-eslint/no-this-alias': 'off',
+        },
+    },
 ]);
